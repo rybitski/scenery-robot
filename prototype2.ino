e1d0359ac2084b2341c1fe19489fd6e37d2bb895
brb7nu <brb7nu@virginia.edu>
2016-01-08 14:59:57 -0500

    Start prototype code

--- prototype2.ino
+++ prototype2.ino
@@ -1,10 +1,8 @@
 #include <Servo.h>
 #include <SPI.h>
 #include <EthernetV2_0.h>
-#include <EthernetUdpV2_0.h>
 
-const int LEFT_MOTOR_PWM_PIN = 5;
-const int RIGHT_MOTOR_PWM_PIN = 6;
+const int SDCARD_CS_PIN = 10;
 
 Servo leftMotor;
 Servo rightMotor;
@@ -13,32 +11,20 @@ Servo rightMotor;
 boolean lastConnected = false;
 String currentLine;
 byte mac[] = { 0x90, 0xA2, 0xDA, 0x00, 0xD4, 0xE7 };
-IPAddress myIP;
 IPAddress ip(192, 162, 1, 177);
 
-// set point
-EthernetUdp setPointClient;
+EthernetClient client;
 
 void setup() {
 	// put your setup code here, to run once:
+	Serial.begin(9600);
 
-	pinMode(LEFT_MOTOR_PWM_PIN, OUTPUT);
-	pinMode(RIGHT_MOTOR_PWM_PIN, OUTPUT);
+	// Set chip select high (inactive) for SD card.
+	pinMode(SDCARD_CS_PIN, OUTPUT);
+	digitalWrite(SDCARD_CS_PIN, HIGH);
 
-	leftMotor.attach(LEFT_MOTOR_PWM_PIN);
-	rightMotor.attach(RIGHT_MOTOR_PWM_PIN);
-
-	//get our ip address
-	myIP = Ethernet.localIP();
-
-	//print the local IP address:
-	Serial.print("My IP address: ");
-	for (byte thisByte = 0; thisByte < 4; thisByte++) {
-	  // print the value of each byte of the IP address:
-	  Serial.print(myIP[thisByte], DEC);
-	  Serial.print(".");
-	}
-	Serial.println();
+	// get and print our IP address
+	Serial.println(Ethernet.localIP());
 
 	Serial.println("Done with setup()!");
 }
@@ -46,26 +32,5 @@ void setup() {
 void loop() {
 	// put your main code here, to run repeatedly:
 
-	for (int i = -10; i < 10; i += 1) {
-		Serial.print("Writing ");
-		Serial.print(i);
-		Serial.println(" to the left servo.");
-		setSpeed(&leftMotor, i, 1);
 
-		Serial.print("Writing ");
-		Serial.print(i);
-		Serial.println(" to the right servo.");
-		setSpeed(&rightMotor, i, -1);
-		
-		delay(2000);
-	}
-
-	// uncomment this to calibrate the servo'
-	// setSpeed(&rightMotor, 0, -1);
-	// setSpeed(&leftMotor, 0, 1);
-
-}
-
-void setSpeed(Servo *servo, int speed, int flip) {
-	servo->writeMicroseconds(flip * 2 * speed + 1500);
 }
