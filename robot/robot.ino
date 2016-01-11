4fa553735c230d32f2bfec93852f70e4973dd765
brb7nu <brb7nu@virginia.edu>
2016-01-11 16:52:08 -0500

    fix indentation and formatting. No functionality difference

--- robot/robot.ino
+++ robot/robot.ino
@@ -39,119 +39,117 @@ signed long pathBuffer[BUFFER_SIZE][2]; // for left and right encoder
 unsigned int pathBufferIndex = 0;
 
 void initEncoders() {
-  
-  // Set slave selects as outputs
-  pinMode(slaveSelectEnc1, OUTPUT);
-  pinMode(slaveSelectEnc2, OUTPUT);
-  
-  // Raise select pins
-  // Communication begins when you drop the individual select signsl
-  digitalWrite(slaveSelectEnc1,HIGH);
-  digitalWrite(slaveSelectEnc2,HIGH);
-  
-  SPI.begin();
-  
-  // Initialize encoder 1
-  //    Clock division factor: 0
-  //    Negative index input
-  //    free-running count mode
-  //    x4 quatrature count mode (four counts per quadrature cycle)
-  // NOTE: For more information on commands, see datasheet
-  digitalWrite(slaveSelectEnc1,LOW);        // Begin SPI conversation
-  SPI.transfer(0x88);                       // Write to MDR0
-  SPI.transfer(0x03);                       // Configure to 4 byte mode
-  digitalWrite(slaveSelectEnc1,HIGH);       // Terminate SPI conversation 
+	// Set slave selects as outputs
+		pinMode(slaveSelectEnc1, OUTPUT);
+		pinMode(slaveSelectEnc2, OUTPUT);
 
-  // Initialize encoder 2
-  //    Clock division factor: 0
-  //    Negative index input
-  //    free-running count mode
-  //    x4 quatrature count mode (four counts per quadrature cycle)
-  // NOTE: For more information on commands, see datasheet
-  digitalWrite(slaveSelectEnc2,LOW);        // Begin SPI conversation
-  SPI.transfer(0x88);                       // Write to MDR0
-  SPI.transfer(0x03);                       // Configure to 4 byte mode
-  digitalWrite(slaveSelectEnc2,HIGH);       // Terminate SPI conversation 
+	// Raise select pins
+	// Communication begins when you drop the individual select signsl
+		digitalWrite(slaveSelectEnc1,HIGH);
+		digitalWrite(slaveSelectEnc2,HIGH);
+
+		SPI.begin();
+
+	// Initialize encoder 1
+	//    Clock division factor: 0
+	//    Negative index input
+	//    free-running count mode
+	//    x4 quatrature count mode (four counts per quadrature cycle)
+	// NOTE: For more information on commands, see datasheet
+	digitalWrite(slaveSelectEnc1,LOW);        // Begin SPI conversation
+	SPI.transfer(0x88);                       // Write to MDR0
+	SPI.transfer(0x03);                       // Configure to 4 byte mode
+	digitalWrite(slaveSelectEnc1,HIGH);       // Terminate SPI conversation 
+
+	// Initialize encoder 2
+	//    Clock division factor: 0
+	//    Negative index input
+	//    free-running count mode
+	//    x4 quatrature count mode (four counts per quadrature cycle)
+	// NOTE: For more information on commands, see datasheet
+	digitalWrite(slaveSelectEnc2,LOW);        // Begin SPI conversation
+	SPI.transfer(0x88);                       // Write to MDR0
+	SPI.transfer(0x03);                       // Configure to 4 byte mode
+	digitalWrite(slaveSelectEnc2,HIGH);       // Terminate SPI conversation 
 }
 
 long readEncoder(int encoder) {
-  
-  // Initialize temporary variables for SPI read
-  unsigned int count_1, count_2, count_3, count_4;
-  long count_value;  
-  
-  // Read encoder 1
-  if (encoder == 1) {
-    digitalWrite(slaveSelectEnc1,LOW);      // Begin SPI conversation
-    SPI.transfer(0x60);                     // Request count
-    count_1 = SPI.transfer(0x00);           // Read highest order byte
-    count_2 = SPI.transfer(0x00);           
-    count_3 = SPI.transfer(0x00);           
-    count_4 = SPI.transfer(0x00);           // Read lowest order byte
-    digitalWrite(slaveSelectEnc1,HIGH);     // Terminate SPI conversation 
-  }
-  
-  // Read encoder 2
-  else if (encoder == 2) {
-    digitalWrite(slaveSelectEnc2,LOW);      // Begin SPI conversation
-    SPI.transfer(0x60);                      // Request count
-    count_1 = SPI.transfer(0x00);           // Read highest order byte
-    count_2 = SPI.transfer(0x00);           
-    count_3 = SPI.transfer(0x00);           
-    count_4 = SPI.transfer(0x00);           // Read lowest order byte
-    digitalWrite(slaveSelectEnc2,HIGH);     // Terminate SPI conversation 
-  }
-  
-  // Calculate encoder count
-  count_value = (count_1 << 8) + count_2;
-  count_value = (count_value << 8) + count_3;
-  count_value = (count_value << 8) + count_4;
-  
-  return count_value;
+
+	// Initialize temporary variables for SPI read
+	unsigned int count_1, count_2, count_3, count_4;
+	long count_value;  
+
+	// Read encoder 1
+	if (encoder == 1) {
+		digitalWrite(slaveSelectEnc1,LOW);      // Begin SPI conversation
+		SPI.transfer(0x60);                     // Request count
+		count_1 = SPI.transfer(0x00);           // Read highest order byte
+		count_2 = SPI.transfer(0x00);           
+		count_3 = SPI.transfer(0x00);           
+		count_4 = SPI.transfer(0x00);           // Read lowest order byte
+		digitalWrite(slaveSelectEnc1,HIGH);     // Terminate SPI conversation 
+	}
+
+	// Read encoder 2
+	else if (encoder == 2) {
+		digitalWrite(slaveSelectEnc2,LOW);      // Begin SPI conversation
+		SPI.transfer(0x60);                      // Request count
+		count_1 = SPI.transfer(0x00);           // Read highest order byte
+		count_2 = SPI.transfer(0x00);           
+		count_3 = SPI.transfer(0x00);           
+		count_4 = SPI.transfer(0x00);           // Read lowest order byte
+		digitalWrite(slaveSelectEnc2,HIGH);     // Terminate SPI conversation 
+	}
+
+	// Calculate encoder count
+	count_value = (count_1 << 8) + count_2;
+	count_value = (count_value << 8) + count_3;
+	count_value = (count_value << 8) + count_4;
+
+	return count_value;
 }
 
 void clearEncoderCount() {
-    
-  // Set encoder1's data register to 0
-  digitalWrite(slaveSelectEnc1,LOW);      // Begin SPI conversation  
-  // Write to DTR
-  SPI.transfer(0x98);    
-  // Load data
-  SPI.transfer(0x00);  // Highest order byte
-  SPI.transfer(0x00);           
-  SPI.transfer(0x00);           
-  SPI.transfer(0x00);  // lowest order byte
-  digitalWrite(slaveSelectEnc1,HIGH);     // Terminate SPI conversation 
-  
-  delayMicroseconds(100);  // provides some breathing room between SPI conversations
-  
-  // Set encoder1's current data register to center
-  digitalWrite(slaveSelectEnc1,LOW);      // Begin SPI conversation  
-  SPI.transfer(0xE0);    
-  digitalWrite(slaveSelectEnc1,HIGH);     // Terminate SPI conversation   
-  
-  // Set encoder2's data register to 0
-  digitalWrite(slaveSelectEnc2,LOW);      // Begin SPI conversation  
-  // Write to DTR
-  SPI.transfer(0x98);    
-  // Load data
-  SPI.transfer(0x00);  // Highest order byte
-  SPI.transfer(0x00);           
-  SPI.transfer(0x00);           
-  SPI.transfer(0x00);  // lowest order byte
-  digitalWrite(slaveSelectEnc2,HIGH);     // Terminate SPI conversation 
-  
-  delayMicroseconds(100);  // provides some breathing room between SPI conversations
-  
-  // Set encoder2's current data register to center
-  digitalWrite(slaveSelectEnc2,LOW);      // Begin SPI conversation  
-  SPI.transfer(0xE0);    
-  digitalWrite(slaveSelectEnc2,HIGH);     // Terminate SPI conversation 
+	// Set encoder1's data register to 0
+	digitalWrite(slaveSelectEnc1,LOW);      // Begin SPI conversation  
+	// Write to DTR
+	SPI.transfer(0x98);    
+	// Load data
+	SPI.transfer(0x00);  // Highest order byte
+	SPI.transfer(0x00);           
+	SPI.transfer(0x00);           
+	SPI.transfer(0x00);  // lowest order byte
+	digitalWrite(slaveSelectEnc1,HIGH);     // Terminate SPI conversation 
+
+	delayMicroseconds(100);  // provides some breathing room between SPI conversations
+
+	// Set encoder1's current data register to center
+	digitalWrite(slaveSelectEnc1,LOW);      // Begin SPI conversation  
+	SPI.transfer(0xE0);    
+	digitalWrite(slaveSelectEnc1,HIGH);     // Terminate SPI conversation   
+
+	// Set encoder2's data register to 0
+	digitalWrite(slaveSelectEnc2,LOW);      // Begin SPI conversation  
+	// Write to DTR
+	SPI.transfer(0x98);    
+	// Load data
+	SPI.transfer(0x00);  // Highest order byte
+	SPI.transfer(0x00);           
+	SPI.transfer(0x00);           
+	SPI.transfer(0x00);  // lowest order byte
+	digitalWrite(slaveSelectEnc2,HIGH);     // Terminate SPI conversation 
+
+	delayMicroseconds(100);  // provides some breathing room between SPI conversations
+
+	// Set encoder2's current data register to center
+	digitalWrite(slaveSelectEnc2,LOW);      // Begin SPI conversation  
+	SPI.transfer(0xE0);    
+	digitalWrite(slaveSelectEnc2,HIGH);     // Terminate SPI conversation 
 }
 
 void setup() {
 	Serial.begin(9600);
-	
+
 	Serial.println("Begin setup()");
 	Serial.println("We are the client!");
 
@@ -195,14 +193,14 @@ void loop() {
 	// Retrieve current encoder counters
 	encoder1count = readEncoder(1); 
 	encoder2count = readEncoder(2);
-	
+
 	Serial.print("Enc1: "); Serial.print(encoder1count); Serial.print(" Enc2: "); Serial.println(encoder2count); 
 
 	// if there are incoming bytes available 
 	// from the server, read them and print them:
 	if (client.available()) {
-	  char c = client.read();
-	  Serial.print(c);
+		char c = client.read();
+		Serial.print(c);
 	}
 
 	// send the recorded path buffer back to the server
@@ -215,11 +213,11 @@ void loop() {
 
 	// if the server's disconnected, stop the client:
 	if (!client.connected()) {
-	  Serial.println();
-	  Serial.println("Server has disconnected. Stopping client.");
-	  client.stop();
+		Serial.println();
+		Serial.println("Server has disconnected. Stopping client.");
+		client.stop();
 
-	  // do nothing forevermore:
-	  while(true);
+		// do nothing forevermore:
+		while(true);
 	}
 }
