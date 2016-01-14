b25dffe0008b6f01c78960a123058f73ad4aca83
smj2mm <smj2mm@virginia.edu>
2016-01-14 12:12:05 -0500

    fixed stuff...

--- server_connect_dialog.py
+++ server_connect_dialog.py
@@ -1,10 +1,6 @@
 # -*- coding: utf-8 -*-
 
-<<<<<<< HEAD
 # Form implementation generated from reading ui file '.\server_connect_dialog.ui'
-=======
-# Form implementation generated from reading ui file 'server_connect_dialog.ui'
->>>>>>> 689bdfba5b02b904d5059f5d60a1174287f2030f
 #
 # Created by: PyQt4 UI code generator 4.11.4
 #
@@ -26,52 +22,52 @@ except AttributeError:
     def _translate(context, text, disambig):
         return QtGui.QApplication.translate(context, text, disambig)
 
-class Ui_ServerEntry(object):
-    def setupUi(self, ServerEntry):
-        ServerEntry.setObjectName(_fromUtf8("ServerEntry"))
-        ServerEntry.resize(400, 107)
-        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Preferred, QtGui.QSizePolicy.Minimum)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(ServerEntry.sizePolicy().hasHeightForWidth())
-        ServerEntry.setSizePolicy(sizePolicy)
-        self.gridLayout = QtGui.QGridLayout(ServerEntry)
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName(_fromUtf8("Dialog"))
+        Dialog.resize(400, 119)
+        self.gridLayout = QtGui.QGridLayout(Dialog)
         self.gridLayout.setObjectName(_fromUtf8("gridLayout"))
-        self.buttonBox = QtGui.QDialogButtonBox(ServerEntry)
+        self.buttonBox = QtGui.QDialogButtonBox(Dialog)
         self.buttonBox.setOrientation(QtCore.Qt.Horizontal)
         self.buttonBox.setStandardButtons(QtGui.QDialogButtonBox.Cancel|QtGui.QDialogButtonBox.Ok)
         self.buttonBox.setObjectName(_fromUtf8("buttonBox"))
         self.gridLayout.addWidget(self.buttonBox, 2, 0, 1, 1)
-        self.frame = QtGui.QFrame(ServerEntry)
-        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Preferred, QtGui.QSizePolicy.Minimum)
+        self.frame = QtGui.QFrame(Dialog)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Preferred, QtGui.QSizePolicy.Preferred)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.frame.sizePolicy().hasHeightForWidth())
         self.frame.setSizePolicy(sizePolicy)
-        self.frame.setMinimumSize(QtCore.QSize(0, 60))
         self.frame.setFrameShape(QtGui.QFrame.StyledPanel)
         self.frame.setFrameShadow(QtGui.QFrame.Raised)
         self.frame.setObjectName(_fromUtf8("frame"))
+        self.gridLayout_2 = QtGui.QGridLayout(self.frame)
+        self.gridLayout_2.setObjectName(_fromUtf8("gridLayout_2"))
         self.label = QtGui.QLabel(self.frame)
-        self.label.setGeometry(QtCore.QRect(10, 0, 221, 16))
         font = QtGui.QFont()
         font.setPointSize(10)
         self.label.setFont(font)
         self.label.setObjectName(_fromUtf8("label"))
+        self.gridLayout_2.addWidget(self.label, 0, 0, 1, 1)
         self.textEdit = QtGui.QTextEdit(self.frame)
-        self.textEdit.setGeometry(QtCore.QRect(10, 20, 361, 31))
-        font = QtGui.QFont()
-        font.setPointSize(10)
-        self.textEdit.setFont(font)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.textEdit.sizePolicy().hasHeightForWidth())
+        self.textEdit.setSizePolicy(sizePolicy)
+        self.textEdit.setMinimumSize(QtCore.QSize(0, 30))
+        self.textEdit.setMaximumSize(QtCore.QSize(16777215, 30))
         self.textEdit.setObjectName(_fromUtf8("textEdit"))
+        self.gridLayout_2.addWidget(self.textEdit, 1, 0, 1, 1)
         self.gridLayout.addWidget(self.frame, 1, 0, 1, 1)
 
-        self.retranslateUi(ServerEntry)
-        QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL(_fromUtf8("accepted()")), ServerEntry.accept)
-        QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL(_fromUtf8("rejected()")), ServerEntry.reject)
-        QtCore.QMetaObject.connectSlotsByName(ServerEntry)
+        self.retranslateUi(Dialog)
+        QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL(_fromUtf8("accepted()")), Dialog.accept)
+        QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL(_fromUtf8("rejected()")), Dialog.reject)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
 
-    def retranslateUi(self, ServerEntry):
-        ServerEntry.setWindowTitle(_translate("ServerEntry", "Dialog", None))
-        self.label.setText(_translate("ServerEntry", "Enter server address and press OK", None))
+    def retranslateUi(self, Dialog):
+        Dialog.setWindowTitle(_translate("Dialog", "Dialog", None))
+        self.label.setText(_translate("Dialog", "Enter server address and press OK", None))
 
